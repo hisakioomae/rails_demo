583053877dd6c3366a17c84ad895ab7fafbe5444
syo <roto1618@gmail.com>
2017-06-20 13:59:47 +0900

    add header content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def top
   end
+  def about
+
+  end
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
+  get "about" => "home#about"
+  get "contact" => "home#contact"
+  get 'post/index' => "post#index"
+  get "post/new" => "postnew"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
