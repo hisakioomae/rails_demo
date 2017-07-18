d114bf1d6585d66eaae336cf8ec3b0d4d481990f
syo <roto1618@gmail.com>
2017-07-18 13:56:36 +0900

    create Like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708160818) do
+ActiveRecord::Schema.define(version: 20170718043714) do
 
   create_table "coments", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170708160818) do
     t.string "picture"
     t.string "image"
     t.integer "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "picts", force: :cascade do |t|
