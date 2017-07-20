6545d0452f6a458f9cde8bc109edca8608068ba2
syo <roto1618@gmail.com>
2017-07-20 14:02:41 +0900

    fix rename

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170718043714) do
 
-  create_table "coments", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "picture"
-    t.string "image"
-    t.integer "user_id"
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -28,10 +19,11 @@ ActiveRecord::Schema.define(version: 20170718043714) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "picts", force: :cascade do |t|
-    t.string "picture"
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
