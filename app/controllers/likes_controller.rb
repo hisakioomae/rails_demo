1649021554b70f706786ea1ed272f2e33ba4ce08
syo <roto1618@gmail.com>
2017-07-20 14:45:37 +0900

    add いいね！削除機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,4 +7,10 @@ class LikesController < ApplicationController
     redirect_to("/posts/#{params[:post_id]}")
   end
 
+  def destroy
+    @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
+    @like.destroy
+    redirect_to("/posts/#{params[:post_id]}")
+  end
+
 end
