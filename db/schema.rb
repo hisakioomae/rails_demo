2077898c2e52758c5109c69b95002bcf4235c826
中西 康平 <nakanishikohei@LAB-N1225.local>
2017-07-26 12:23:02 +0900

    change readme

--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,7 @@ ActiveRecord::Schema.define(version: 20170708160818) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
-    t.string "image"
     t.integer "user_id"
-  end
-
-  create_table "picts", force: :cascade do |t|
-    t.string "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
